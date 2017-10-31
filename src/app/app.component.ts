a139900059c7bdb82cf9b638a54044b288302556
Kwant <Kwant.matthieu@gmail.com>
2017-10-31 15:22:19 +0100

    succes de l'exercice sur Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  
+  etudiants: Array<any> // Array<string>
+  constructor() {
+  	this.etudiants = [
+  		{
+  		nom: 'Pierre',
+  		moyenne: '15',
+  		admis: true
+  		} , {
+
+  		nom: 'Paul',
+  		moyenne: '10',
+  		admis: true
+
+  		} ,	{
+
+  		nom: 'Jacques',
+  		moyenne: '8',
+  		admis: false
+
+  		} ,	{
+
+  		nom: 'Manon',
+  		moyenne: '5',
+  		admis: false
+  		} 
+
+  	];
+  }
 }
