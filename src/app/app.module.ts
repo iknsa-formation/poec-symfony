17922b99f0ebed1a2ea3e26dadc4b11fba4bb8dc
Kwant <Kwant.matthieu@gmail.com>
2017-10-31 17:43:45 +0100

    ajouté des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SectionComponent } from './section/section.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SectionComponent,
+    BodyComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
