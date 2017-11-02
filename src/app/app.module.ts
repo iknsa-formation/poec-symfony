e6a0ff47b7b839634fd99754e4d6639d08b3415c
jeremy10tie <jeremy.gillot@gmail.com>
2017-11-02 17:10:33 +0100

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { MenuComponent } from './menu/menu.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes:Routes = [
+{
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'demo',
+    component: DemoComponent
+  },
+   
+    
+]
 
 @NgModule({
   declarations: [
@@ -15,14 +30,20 @@ import { FooterComponent } from './footer/footer.component';
     DemoComponent,
     MenuComponent,
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+	RouterModule.forRoot(appRoutes)
+	
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
