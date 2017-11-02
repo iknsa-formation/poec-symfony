a8456284913c85afb79c7322650f6faf5c31e739
Kwant <Kwant.matthieu@gmail.com>
2017-11-02 16:40:31 +0100

    Deuxième TP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,32 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  etudiants: Array<any> // Array<string>
-  constructor() {
-  	this.etudiants = [
-  		{
-  		nom: 'Pierre',
-  		moyenne: '15',
-  		admis: true
-  		} , {
 
-  		nom: 'Paul',
-  		moyenne: '10',
-  		admis: true
-
-  		} ,	{
-
-  		nom: 'Jacques',
-  		moyenne: '8',
-  		admis: false
-
-  		} ,	{
-
-  		nom: 'Manon',
-  		moyenne: '5',
-  		admis: false
-  		} 
-
-  	];
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,26 @@ import { HeaderComponent } from './header/header.component';
 import { SectionComponent } from './section/section.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { RouterModule, Routes } from '@angular/router'; 
+
+const appRoutes: Routes = [
+{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'about',
+  component: AboutComponent
+},
+{
+  path: 'contact',
+  component: ContactComponent
+}
+];
 
 @NgModule({
   declarations: [
@@ -16,8 +36,13 @@ import { FooterComponent } from './footer/footer.component';
     SectionComponent,
     BodyComponent,
     FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
+
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
