7342880062214d0b70ff22520834a28c0ad78f06
womendy <solangemendy@aol.fr>
2017-11-02 13:10:30 +0100

    ajout menu vertical, contenur et sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContenuComponent } from './contenu/contenu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ContenuComponent
   ],
   imports: [
     BrowserModule
