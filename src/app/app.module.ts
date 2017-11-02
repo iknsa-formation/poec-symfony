4e50cbfe9f34e268c698620a4b7d7409ef6911e2
FthierryF <thi.fendian@gmail.com>
2017-11-02 12:18:23 +0100

    mon tp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TableauComponent } from './tableau/tableau.component';
+import { DemoComponent } from './demo/demo.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableauComponent,
+    DemoComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
