578cd3f0a76867dd37477c4b7bc1d4d98793bc30
womendy <solangemendy@aol.fr>
2017-11-02 17:06:47 +0100

    test routes dans header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,27 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContenuComponent } from './contenu/contenu.component';
+import {RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes =[ 
+{
+	path: '',
+component: HomeComponent
+},
+
+{
+path: 'about',
+component: AboutComponent
+},
+
+{
+path: 'contact',
+component: ContactComponent
+}
+] ;
 
 
 @NgModule({
@@ -14,12 +35,18 @@ import { ContenuComponent } from './contenu/contenu.component';
     FooterComponent,
     HeaderComponent,
     SidebarComponent,
-    ContenuComponent
+    ContenuComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
