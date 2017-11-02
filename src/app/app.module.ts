8086e729020bd3aa253fd7001b79054558b3dc97
jeremy10tie <jeremy.gillot@gmail.com>
2017-11-02 11:24:27 +0100

    projet composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    MenuComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
