2ba8a63848314a03ddb32a852ed54fa0d397c16e
Arokiyaraja <aland.arokiyaraja@hotmail.com>
2017-11-02 11:37:49 +0100

    tp-aro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    LoginComponent,
+    SidebarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
