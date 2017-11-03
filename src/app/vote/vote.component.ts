5895d31a9d97353e78a15ed8920b18f2392e3cb9
Abdillah <abdillah.mohamed@hotmail.fr>
2017-11-03 16:27:43 +0100

    Ajout d'une liste sur le composant (vote) + event sur bouton et lien

---
+++ src/app/vote/vote.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vote',
+  templateUrl: './vote.component.html',
+  styleUrls: ['./vote.component.css']
+})
+export class VoteComponent implements OnInit {
+    listeLiens:Array<any>;
+  constructor() {
+  this.listeLiens = [];
+  }
+ajoutLien(lien,titre){
+    let unLien = lien;
+    let unTitre = titre;
+    this.listeLiens.push({"lien":lien,"titre":titre,"vote":0});
+    alert(this.listeLiens.length);
+}
+    ajoutVoix(unIndice){
+        this.listeLiens[unIndice].vote++;
+    }
+    supprimerVoix(unIndice){
+        this.listeLiens[unIndice].vote--;
+    }
+  ngOnInit() {
+  }
+
+}
