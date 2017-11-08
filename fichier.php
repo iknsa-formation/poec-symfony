cf7a1b2e47d98906a45e1be6bc36a73a0a957d67
Moussa Camara <moussa.camara@pleaky.com>
2017-11-08 16:58:58 +0100

    insertion base de donnée avec 2 fichiers séparés

--- fichier.php
+++ fichier.php
@@ -1,63 +1,38 @@
 <?php
+define('DBSERVER', 'localhost');
+define('DBNAME', 'poec-symfony');
+define('DBUSER', 'root');
+define('DBPASS', '');
 
-    $Prenom = 'Moussa';
-    $Nom = 'Camara';
-    $Age    = '28 ans';
-    $Profession = 'informaticien';
+$connection = new mysqli(DBSERVER, DBUSER, DBPASS, DBNAME);
 
+     if (!$connection) {
+        die('<strong> Impossible de se possible, veuillez revoir les accés</strong><br>');
+    } else {
+
+        echo '<strong> Super ! j\'ai réussi à me connecter à ma base de donnée </strong><br>';
+    }
+
+    if (isset($_POST['firstname']) &&  isset($_POST['lastname']) &&  isset($_POST['adresse']) &&  isset($_POST['fonction'])){
+
+        $prenom = $_POST['firstname'];
+        $nom = $_POST['lastname'];
+        $adresse = $_POST['adresse'];
+        $fonction = $_POST['fonction'];
+
+        echo "Prénom: " .$prenom . "<br>";
+        echo "Nom: " .$nom;
+        echo "Adresse: " .$adresse;
+        echo "Fonction: " .$fonction;
+
+        /*
+        * Instertion dans la base de donnée
+        */
+        
+        $sql = "INSERT INTO users(firstname,lastname,adresse,fonction) VALUES('".$prenom."','".$nom."','".$adresse."','".$fonction."')";
+        
+        mysqli_query ($connection,$sql) or die ('Erreur SQL !'.$sql.'<br />'.mysql_error());
+
+        mysqli_close($connection);
+    }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>JavaScript Course - IKNSA</title>
-    <link rel="stylesheet" href="css/styles.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
-  </head>
-  <body>
-    <!-- Page Content -->
-    <header>
-        <div class="logo"><img src="img/logo.png" alt="Logo"></div>
-        <nav>
-            <ul>
-                <li><a href="index.html">Home Page</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="contact.html">Contact Us</a></li>
-            </ul>
-        </nav>
-
-        <div class="breadcrumb">
-            <span>You are here</span>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="page.html">About Us</a></li>
-            </ul>
-        </div>
-    </header>
-
-     <div class="jumbotron">
-
-        <?php
-
-            function direBonjour(){
-                echo "Bonjour tout le monde !";
-            }
-
-            direBonjour();
-
-            function addition($nombre1, $nombre2){
-                $resultat=$nombre1 + $nombre2;
-                return $resultat;
-            }
-
-           echo addition(15,1);
-        ?>
-     </div>
-
-
- <footer>
-        copyright @iknsa.com
-    </footer>
-  </body>
-</html>
