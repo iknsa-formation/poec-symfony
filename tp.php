13915db57c0cbf32b7d345501115a607ba3f2dc4
Kwant <Kwant.matthieu@gmail.com>
2017-11-14 18:20:29 +0100

    Ajout de la connectiojn à la BDD

--- tp.php
+++ tp.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,26 +8,41 @@
 	<title>TP PHP</title>
 	<!-- Bootstrap 4 CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
 </head>
 
 <body>
 
 <?php
 
-$contact = array (
+define('DBSERVER', 'localhost');
+define('DBNAME','Contact');
+define('DBUSER', 'root');
+define('DBPWD', '');
+
+$connection = mysqli_connect(DBSERVER, DBUSER ,DBPWD, DBNAME);
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+$_SESSION['contact'] = array (
 	1 => array("prenom" => "Moussa", "nom" => "Camara", "telephone" => "0120556406"),
 	2 => array("prenom" => "Khalid", "nom" => "Sookia", "telephone" => "0154454421"),
 	3 => array("prenom" => "Moustakime", "nom" => "Kifya", "telephone" => "0698536587"),
 );
 
-echo $contact[2]["prenom"];
+
 ?>
 
 <div class="col-md-6">
-	<table class="table table-striped table-hover">
+	<table id="myTable" class="table table-striped table-hover">
 		<thead class="thead-dark">
 			<tr>
-				<th>prenom</th>
+			    <th>prenom</th>
 				<th>nom</th>
 				<th>tel</th>
 			</tr>
@@ -32,15 +50,13 @@ echo $contact[2]["prenom"];
 
 		<tbody>
 		<?php
-
-
-		foreach ($contact as $key) {
-			echo "<tr> ";
-			foreach ($key as $value) {
-		       echo "<td>" . $value . "</td>";  
-		  	}
-		  	echo "</tr>";
-		}
+			foreach ($_SESSION['contact'] as $key) {
+				echo "<tr> ";
+				foreach ($key as $value) {
+			       echo "<td>" . $value . "</td>";  
+			  	}
+			  	echo "</tr>";
+			}
 		?>
 		</tbody>
 	</table>
@@ -51,6 +67,7 @@ echo $contact[2]["prenom"];
 require 'form.php';
 ?>
 
-
+<!-- Contrôle du formulaire -->
+<script type="text/javascript" src="js/formControl.js"></script>
 </body>
 </html>
