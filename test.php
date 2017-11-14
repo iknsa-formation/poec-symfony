e3c6578b4e07cacb6b5d2fdfbe07b1180587e658
jeremy10tie <jeremy.gillot@gmail.com>
2017-11-14 17:23:46 +0100

    tp php + require

---
+++ test.php
@@ -0,0 +1,8 @@
+<div style="border:2px solid black;border-radius:5%;background-color:lightblue;text-align:center"><b><?php
+
+$test = array('fleur','rouge','vert','orange');
+
+	foreach($test as $value){
+		echo $value."<br>";
+	}
+	?></b></div>
