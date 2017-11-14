4f934f29277de742f9b69752de1a7ec90458cce3
blakim_y <yannickblakime@gmail.com>
2017-11-14 15:34:21 +0100

    add_php2

---
+++ php2/hello_world.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+<?php
+	include 'C:\wamp64\www\first_dir\composants\navbar.php';
+ 	include 'C:\wamp64\www\first_dir\composants\header.html';
+ 	echo "hello_world";
+?> 
+</body>
+</html>
+<php
---
+++ php2/read_array.php
@@ -0,0 +1,33 @@
+<?php
+	 	include 'C:\wamp64\www\php\components\navbar.php';
+	 	include 'C:\wamp64\www\php\components\header.html';
+?>
+
+<body>
+
+<div class="content">
+	<?php
+	 		$fruit = array("pomme","banane","abricot");
+
+	 		for ($i=0; $i < 3; $i++) 
+	 		{ 
+				echo("<br>");	 			
+	 			echo($fruit["$i"]);	
+	 		}
+
+	 		$test = array('test1' => 'sécurité_du_code', 'test2' => 'fonctionalité_de_lappli');
+?>
+<br>
+<?php 
+	 		foreach ($test as $key => $value) 
+	 		{
+	 			echo("<br>");
+	 			echo "le test niumero $key est le test de $value";
+	 		}
+	?>	
+</div>
+
+
+</body>
+</html>
+<php
