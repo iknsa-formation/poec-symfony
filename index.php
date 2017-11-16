64696b77886b5413ef933af9605a62ff3ed2dd06
Abdillah <abdillah.mohamed@hotmail.fr>
2017-11-16 10:45:11 +0100

    Réorganistion du html en pages

--- index.php
+++ index.php
@@ -4,49 +4,6 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	</head>
 	<body>
-		<span>
-			<h3>Liste des etudiants</h3>
-			<table class="table">
-				<thead class="">
-					<tr>
-						<th>id</th>
-						<th>prenom</th>
-						<th>nom</th>
-						<th>moyen</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php listEtu(listeEtudiantExam()); ?>
-				</tbody>
-			</table>
-
-			<h3>Liste des admis</h3>
-			<table class="table">
-				<thead class="">
-					<tr>
-						<th>id</th>
-						<th>prenom</th>
-						<th>nom</th>
-						<th>moyen</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php trieEtuAdmis(listeEtudiantExam()); ?>
-				</tbody>
-			</table>
-
-			<h3>Liste des non admis</h3>
-			<table class="table">
-				<thead class="">
-					<th>id</th>
-					<th>prenom</th>
-					<th>nom</th>
-					<th>moyen</th>
-				</thead>
-				<tbody>
-					<?php trieEtuNonAdmis(listeEtudiantExam()); ?>
-				</tbody>
-			</table>
-		</span>
+		<?php include_once "pages/etudiants.html"; ?>
 	</body>
 <html>
