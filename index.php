573558d26917d387248e481159cf264f0add7d19
FthierryF <thi.fendian@gmail.com>
2017-11-17 17:03:58 +0100

    liens vers les autre fichiers

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
-<p>Tp formulaire
-  <link rel="stylesheet" href="Fort_Muller.php">
+<?php
+echo "<a href=\"Tablo2.php\">Tablo2</a><br/>";
+echo "<a href=\"Fort_Muller.php\">le formulaire</a><br/>";
+echo "<a href=\"db.php\">db</a><br/>";
+?>
