43e2da5424da71df8b8bb027fd972c3cba62fdb8
FthierryF <thi.fendian@gmail.com>
2017-11-17 11:22:39 +0100

    requettes OK

--- Fort_Muller.php
+++ Fort_Muller.php
@@ -1,6 +1,4 @@
-<?php
-require_once "db.php"
-?>
+
 <!DOCTYPE html>
 <html>
   <head>
--- fichier.php
+++ fichier.php
@@ -1,16 +1,34 @@
 <?php
+require_once "db.php";
+if (count($_POST) >0){
+
+
 $nom = $_POST ['lastname'];
 $prenom = $_POST ['firstname'];
 $adresse = $_POST ['adresse'];
 $fonction = $_POST ['fonction'];
-if (count($_POST) >0){
+$isExist = "select lastname from user where lastname='".$nom."'";
+// var_dump($nom);die;
 
-
+$exist = mysqli_query ($connection, $isExist);
+// var_dump($isExist);die;
+$var = mysqli_num_rows ($exist);
+var_dump($exist);die;
     echo "nom : ".$nom."<br/>";
     echo "prenom : ".$prenom."<br/>";
     echo "adresse : ".$adresse."<br/>";
     echo "fonction : ".$fonction."<br/>";
-}
-$sql = "INSERT INTO users(firstname,lastname,adresse,fonction)
+
+$sql = "INSERT INTO user(firstname,lastname,adresse,fonction)
 VALUES('".$prenom."', '".$nom."','".$adresse."', '".$fonction."')";
+$insert = mysqli_query ($connection,$sql);
+
+if ($insert) {
+  echo "<div class= 'alert alert-succeess' role= 'alert'>
+  L'utilisateur $prenom $nom a été ajouté en base! </div>";
+    }
+}
 ?>
+
+
+<!-- $add = mysqli_query ($connection,,$addCell); -->
