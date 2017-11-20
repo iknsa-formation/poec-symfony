f514eaa2b672fd306098c2bdfdd5a20b97a7d66f
bina <yebda.sabrina@gmail.com>
2017-11-20 12:10:41 +0100

    moyenne

---
+++ moyenne/foreach1.php
@@ -0,0 +1,11 @@
+<?php
+$eleve= array(
+1 => array('id'=>'1','prenom' => 'Lana', 'nom' => 'ANGEL', 'Moyenne' => '17'),
+2 => array('id'=>'2','prenom' => 'David', 'nom' => 'SMITH', 'Moyenne' => '10'),
+3 => array('id'=>'3','prenom' => 'Thanina', 'nom' => 'BROWNB', 'Moyenne' => '7'),
+4=> array('id'=>'4','prenom' => 'Joe' , 'nom'=>'WHATSON' ,'Moyenne'=> '15'),
+5=> array('id'=>'5','prenom' => 'Nova','nom'=>'TERRA','Moyenne' => '9'),
+6=> array('id'=>'6','prenom' => 'Marie' ,'nom'=>'TABAS','Moyenne' => '19'),
+7=> array('id'=>'7','prenom' => 'Leroy' ,'nom'=>'GIBS','Moyenne' => '11'),
+8=> array('id'=>'8','prenom' => 'Atkins' ,'nom'=>'RANA','Moyenne' => '8')
+);
