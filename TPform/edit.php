9c970231c8695ee23c7a8e809482e2800f6b81ee
eilio <beilionzion@gmail.com>
2017-11-20 17:56:16 +0100

    dumb copy paste

--- TPform/edit.php
+++ TPform/edit.php
@@ -1,40 +1,54 @@
-<?php require_once 'dp.php';
+<?php require_once 'db.php';
 
-$id = $_POST["id"]    
+    if(count($_POST)>0){
+    $id = $_GET["id"];
     
-    var_dump($sql);die;
-
     $sql="UPDATE tablio set name='".$_POST["name"]."',email='".$_POST["email"]."',message='".$_POST["message"]."' WHERE id='".$id."'";
-    
-var_dump($sql);die;
 
-$update = mysqli_query($connection, $sql);
+    $update = mysqli_query($connection, $sql);
+
     if($update){
-?>
+    ?>
+    <div>modification ok</div>
 
-<div>modification ok</div>
-
-<?php
-                
-} else {
-        
+    <?php
+        } else {
         ?>
-<div>Modif KO</div>
+        <div>Modif KO</div>
+    <?php
     }
+}
 
-    $resultat = mysqli_query($connection, $sql);
+    $id = $_GET["id"];
+    $sql = "SELECT * FROM tablio WHERE id= $id";
+        $resultat = mysqli_query($connection, $sql);
 
-            while ($row= mysqli_fetch_object ($resultat) ) {?>
-            <td>
-                <?php echo $row ['id']; ?>
-            </td>
-            <td>
-                <?php echo $row ['name']; ?>
-            </td> 
-            <td>
-                <?php echo $row ['email']; ?>
-            </td> 
-            <td>
-                <?php echo $row ['message']; ?>
-            </td>
-            <td>
+            var_dump("eeeeeee");
+        while ($row= mysqli_fetch_object ($resultat) ) {
+    ?>
+    
+<form action="inputdisplay.php" method="post">
+            <div>
+                <label for="nom">Nom :</label>
+                <input type="text" id="nom" name="user_name" value="<?php echo $row->name;?>">
+            </div>
+
+            <div>
+                <label for="courriel">Courriel :</label>
+                <input type="email" id="courriel" name="user_email" value="<?php echo $row->email;?>">
+            </div>
+
+            <div>
+                <label for="message">Message :</label>
+                <textarea id="message" name="user_message" value="<?php echo $row->message;?>"></textarea>
+            </div>
+
+            <div class="button">
+                <button type="submit">Envoyer</button>
+            </div>
+        </form>
+
+
+    <?php } 
+mysqli_free_result($resultat);
+?>
