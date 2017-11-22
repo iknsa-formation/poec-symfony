d3544173c5558cf456a31814d7c3011975fa75ef
eilio <beilionzion@gmail.com>
2017-11-22 15:04:38 +0100

    hachage ok

--- tpsession/inputdisplay.php
+++ tpsession/inputdisplay.php
@@ -1,23 +1,14 @@
 <?php include 'index.html'?>
 <?php require_once "db.php";?>
 <hr/><?php
-
 var_dump($_POST);
-
 if (count($_POST)>0){
     $name=$_POST["username"];
     $email=$_POST["email"];
-    $password=$_POST["password"];
-    $hash = password_hash ("password", PASSWORD_DEFAULT);
-    
-    
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
     echo "Welcome: ". $_POST["username"]. "<br />";
     echo "Your email is: ". $_POST["email"]. "<br />";
     echo "This is your password: ".password_hash("password", PASSWORD_DEFAULT)."\n"."<br/>";
-
-
-
- 
     $requete="select email from cptable where email='".$email."'";
     $resultat = mysqli_query ($connection, $requete);
 
@@ -34,18 +25,17 @@ if (count($_POST)>0){
 
         $insert = mysqli_query($connection,$sql);
 
-        if ($insert) {
-            echo"ca a marché";
-        }
-        else {
-            echo "ca ne marche pas";
-        }
+        if (mysqli_query($connection,$sql)) {
+    
+echo '<div>
+        bien jouer<meta http-equiv="refresh" content="5; URL=http://localhost/poec/php/poec-symfony/tpsession/list.php"/><div/>';}
+else { echo 'Pluton';}
 
         mysqli_close ($connection);
 
-        
     }
 }
+
 
 
 
