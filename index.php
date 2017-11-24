276f0911ce99ba4d1fa52e55a545909cd59548e7
Abdillah <abdillah.mohamed@hotmail.fr>
2017-11-24 15:45:47 +0100

    TP Namespaces

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	class Article{
+	/*class Article{
 		public $titre = "Deals";
 		public $auteur = "John";
 		const NOMBRE_DE_PAGES = 200;
@@ -48,7 +48,7 @@
 		}
 	}
 
-	/*$unArticle = new Article();
+	$unArticle = new Article();
 	echo "<p> Titre : ".$unArticle->titre."</p>";
 	echo "<p> Auteur : ".$unArticle->auteur."</p>";
 	echo "<p> nombre de pages : ".$unArticle::NOMBRE_DE_PAGES."</p>";
@@ -57,12 +57,26 @@
 	echo "<p> set getTitre : ".$unArticle->setTitre("Headset")."</p>";
 	echo "<p> set getAuteur : ".$unArticle->setAuteur("Malcom")."</p>";
 	echo "<p> méthode getTitre : ".$unArticle->getTitre()."</p>";
-	echo "<p> méthode getAuteur : ".$unArticle->getAuteur()."</p>";*/
+	echo "<p> méthode getAuteur : ".$unArticle->getAuteur()."</p>";
 	$unePersonne = new Personne();
 	$unePersonne->setNom("MOHAMED OMAR");
 	$unePersonne->setPrenom("Abdillah");
 	$unePersonne->setAge(26);
 	echo "<p> méthode getNom : ".$unePersonne->getNom()."</p>";
 	echo "<p> méthode getPrenom : ".$unePersonne->getPrenom()."</p>";
-	echo "<p> méthode getAge : ".$unePersonne->getAge()." ans</p>";
+	echo "<p> méthode getAge : ".$unePersonne->getAge()." ans</p>";*/
+	require 'Personne1.php';
+	require 'Personne2.php';
+	use personne1 as p1;
+	use personne2 as p2;
+
+	$toto = new p1\Personne();
+	$tutu = new p2\Personne();
+
+	echo $toto->setNom("Toto");
+	echo $tutu->setNom("Tutu");
+	echo "<p> méthode getNom de la class Personne depuis name space personne1 : ".$toto->getNom()."</p>";
+	echo $tutu->setSexe("Masculin");
+	echo "<p> méthode getNom de la class Personne depuis name space personne2 : ".$tutu->getNom()."</p>";
+	echo "<p> méthode getSexe de la class Personne depuis name space personne2 : ".$tutu->getSexe()."</p>";
 ?>
