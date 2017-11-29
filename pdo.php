16af5892fab8ecbcda82f061aef01ab7f54bf0c8
Abdillah <abdillah.mohamed@hotmail.fr>
2017-11-29 11:26:50 +0100

    Création d'une class BD + TP Select

--- pdo.php
+++ pdo.php
@@ -1,8 +1,12 @@
 <?php 
-	class UsersManager{
-		public $bd;
+	/**
+	* 
+	*/
+	require 'Bd.php';
+	class UsersManager extends Bd{
+		private $bd;
 		public function __construct (){
-			$this->bd = new PDO('mysql:host=localhost;dbname=poec-symfony', 'root', '');
+			$this->bd = new Bd();
 		}
 		public function insertUser($users){
 			$USERS = $this->bd->prepare('INSERT INTO users(firstname,lastname,adresse,fonction) VALUES (:firstname,:lastname,:adresse,:fonction)');
@@ -18,9 +22,23 @@
 				echo "KOOOOOOOO!";
 			}
 		}
-	}
+		public function selectUsers(){
+			$users = $this->bd->prepare("select * from users");
+			$users->execute();
+			return	$users;
+		}
+		public function afficherUsers(){
+			$user = $this->selectUsers();
 
+			foreach ($user as $key => $row) {
+				echo '<p>Firstname : '.$row['firstname'].'</p>';
+				echo '<p>Lastname : '.$row['lastname'].'</p>';
+			}
+		}
+	}
+	//
 	$art = new UsersManager();
 
-	$art->insertUser(["firstname"=>"Dalton","lastname"=>"Djo","adresse"=>"918 stephen avenue","fonction"=>"Lead dev"]);
+	//$art->insertUser(["firstname"=>"Dalton","lastname"=>"Djo","adresse"=>"918 stephen avenue","fonction"=>"Lead dev"]);
+	$art->afficherUsers();
 ?>
