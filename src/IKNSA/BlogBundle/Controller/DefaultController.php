5c9cfe60f9e77a7707363515a9b734335eee780e
Beronik <you@example.com>
2017-12-11 14:08:09 +0100

    Dossier sauve POST

--- src/IKNSA/BlogBundle/Controller/DefaultController.php
+++ src/IKNSA/BlogBundle/Controller/DefaultController.php
@@ -10,4 +10,9 @@ class DefaultController extends Controller
     {
         return $this->render('IKNSABlogBundle:Default:index.html.twig');
     }
+
+    public function aboutAction()
+    {
+        return $this->render('IKNSABlogBundle:Default:about.html.twig');
+    }
 }
