f291e6135e525021b0029da78b97ba3e8e2ec7d5
Beronik <you@example.com>
2017-12-13 10:23:19 +0100

    TP sympony finir manque le css

--- src/IKNSA/BlogBundle/Entity/User.php
+++ src/IKNSA/BlogBundle/Entity/User.php
@@ -1,24 +1,26 @@
 <?php
 // src/IKNSA/AppBundle/Entity/User.php
 namespace IKNSA\BlogBundle\Entity;
+
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
+
 /**
 * @ORM\Entity
 * @ORM\Table(name="fos_user")
 */
 class User extends BaseUser
 {
-/**
-* @ORM\Id
-* @ORM\Column(type="integer")
-* @ORM\GeneratedValue(strategy="AUTO")
-*/
-protected $id;
-public function __construct()
-{
-parent::__construct();
-// your own logic
-}
+	/**
+	* @ORM\Id
+	* @ORM\Column(type="integer")
+	* @ORM\GeneratedValue(strategy="AUTO")
+	*/
+	protected $id;
+
+	public function __construct()
+	{
+	parent::__construct();
+	// your own logic
+	}
 }
--- src/IKNSA/BlogBundle/Form/PostType.php
+++ src/IKNSA/BlogBundle/Form/PostType.php
@@ -5,8 +5,8 @@ namespace IKNSA\BlogBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
+use Symfony\Component\Form\Extension\Core\Type\TextareaType; 
 
 class PostType extends AbstractType
 {
@@ -15,7 +15,7 @@ class PostType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('summary')->add('content')->add('createdAt')->add('image');
+        $builder->add('title')->add('summary')->add('content')->add('createdAt')->add('file');
     }
     
     /**
